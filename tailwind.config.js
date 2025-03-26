d808c2fa12419b7c2e8767bf4fda0a88e6d826bd
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2025-03-26 17:24:26 +0200

    add files project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    mode: 'jit', // تأكد أن JIT مفعل
+    content: ["./src/**/*.{html,js}"], // تأكد من وضع المسار الصحيح
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
