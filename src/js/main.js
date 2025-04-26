bfa8fe25a8c10bfcf89750d9fe5c937208ab0a30
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2025-04-26 17:28:30 +0300

    add section our-working-process

--- src/js/main.js
+++ src/js/main.js
@@ -13,3 +13,15 @@
     //         btn.style.display = "none";
     //     }
     // }
+
+
+    const btn = document.querySelector(".Our-Working .list1 .open-icon .close-icon");
+    const cards = document.querySelectorAll(".Our-Working .lists-ourWorking .list1");
+
+    cards.forEach((card) => {
+    const btn = card.querySelector(".open-icon");
+
+    btn.addEventListener("click", function () {
+        card.classList.toggle("active");
+    });
+});
