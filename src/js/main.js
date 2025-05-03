56a2930a3e3b81889f56a99fcfc11f2d1eb06365
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2025-05-03 14:46:36 +0300

    add section Testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,87 @@
 
-    // window.addEventListener('scroll', () => {
-    // const scrollY = window.scrollY;
-    // console.log(`Scrolled: ${scrollY}px`);
-    // });
+// window.addEventListener('scroll', () => {
+// const scrollY = window.scrollY;
+// console.log(`Scrolled: ${scrollY}px`);
+// });
 
-    // window.onscroll = function(){
+// window.onscroll = function(){
 
-    //     if(window.scrollY >= 5500){
-    //         btn.style.display = "flex";
-            
-    //     }else{
-    //         btn.style.display = "none";
-    //     }
-    // }
+//     if(window.scrollY >= 5500){
+//         btn.style.display = "flex";
+
+//     }else{
+//         btn.style.display = "none";
+//     }
+// }
 
 
-    const btn = document.querySelector(".Our-Working .list1 .open-icon .close-icon");
-    const cards = document.querySelectorAll(".Our-Working .lists-ourWorking .list1");
+const btn = document.querySelector(".Our-Working .list1 .open-icon .close-icon");
+const cards = document.querySelectorAll(".Our-Working .lists-ourWorking .list1");
 
-    cards.forEach((card) => {
+cards.forEach((card) => {
     const btn = card.querySelector(".open-icon");
 
     btn.addEventListener("click", function () {
         card.classList.toggle("active");
     });
 });
+
+// bulllets active
+
+const bullets = document.querySelectorAll(".Testimonials .testimonials-cards .buttons .bullets i");
+const prev = document.querySelector(".Testimonials .testimonials-cards .buttons .swiper-button-prev i");
+const next = document.querySelector(".Testimonials .testimonials-cards .buttons .swiper-button-next i");
+
+function checkButtons() {
+    let currentIndex = [...bullets].findIndex(b => b.classList.contains("active"));
+
+    if (currentIndex === 0) {
+        prev.classList.add("none");
+    } else {
+        prev.classList.remove("none");
+    }
+
+    if (currentIndex === bullets.length - 1) {
+        next.classList.add("none");
+    } else {
+        next.classList.remove("none");
+    }
+}
+
+function activeBullet() {
+    bullets.forEach(bullet => {
+        bullet.addEventListener("click", (e) => {
+            bullets.forEach(b => b.classList.remove("active"));
+            e.target.classList.add("active");
+
+            checkButtons(); 
+        });
+    });
+}
+
+function NextAndPrev() {
+    next.addEventListener("click", () => {
+        let currentIndex = [...bullets].findIndex(b => b.classList.contains("active"));
+        if (currentIndex < bullets.length - 1) {
+            bullets.forEach(b => b.classList.remove("active"));
+            bullets[currentIndex + 1].classList.add("active");
+
+            checkButtons(); 
+        }
+    });
+
+    prev.addEventListener("click", () => {
+        let currentIndex = [...bullets].findIndex(b => b.classList.contains("active"));
+        if (currentIndex > 0) {
+            bullets.forEach(b => b.classList.remove("active"));
+            bullets[currentIndex - 1].classList.add("active");
+
+            checkButtons(); 
+        }
+    });
+}
+
+activeBullet();
+NextAndPrev();
+checkButtons(); 
+
